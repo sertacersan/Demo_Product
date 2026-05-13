1d20eb995cf86ec7c110d31225916e73deb20346
Sertaç Ersan <kodersan1@gmail.com>
2026-05-13 23:00:43 +0300

    Migration işlemi

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost\\SQLEXPRESS; database=DbNewOopCore1; integrated security=true; TrustServerCertificate=True;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}

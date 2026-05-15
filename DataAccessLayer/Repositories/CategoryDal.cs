d1ca5536a96e956c991a4aa1c6a06aecc3f96a1d
Sertaç Ersan <kodersan1@gmail.com>
2026-05-15 22:44:14 +0300

    Repositories

---
+++ DataAccessLayer/Repositories/CategoryDal.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryDal : ICategoryDal
+    {
+        Context c = new Context();
+        public void Delete(Category p)
+        {
+            c.Categories.Remove(p);
+            c.SaveChanges();
+        }
+
+        public List<Category> GetList()
+        {
+            return c.Categories.ToList();
+        }
+
+        public void Insert(Category p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/ProductDal.cs
@@ -0,0 +1,37 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Repositories
+{
+    public class ProductDal : IProductDal
+    {
+        Context c = new Context();
+        public void Delete(Product p)
+        {
+            c.Remove(p);
+            c.SaveChanges();
+        }
+
+        public List<Product> GetList()
+        {
+            return c.Products.ToList();
+        }
+
+        public void Insert(Product p)
+        {
+            
+            c.Products.Add(p);
+            c.SaveChanges();
+        }
+
+        public void Update(Product p)
+        {
+            c.Products.Update(p);
+            c.SaveChanges();
+        }
+    }
+}
